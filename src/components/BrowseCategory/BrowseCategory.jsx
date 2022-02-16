38cb8113173b8d3b66f8a8c28b76e774a7ae7121
paulchristian <paulchristianmasayon2020@gmail.com>
2022-02-16 12:41:57 +0800

    added category filter functionality

--- src/components/BrowseCategory/BrowseCategory.jsx
+++ src/components/BrowseCategory/BrowseCategory.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 const BrowseCategory = ({filterByCategory, categories}) => {
   // const cat = useSelector(state => state.productCategories)
-  
+  console.log(categories)
   
   return (
     <div className={`${c.categories} ${c.maxwidth500px}`}>
@@ -17,7 +17,7 @@ const BrowseCategory = ({filterByCategory, categories}) => {
             <li
               key={el}
               className={c.li}
-              onClick={() => filterByCategory(el.category)}
+              onClick={() => filterByCategory(el)}
             >
               <Link to={`/products/${el}`}>{el}</Link>
             </li>
